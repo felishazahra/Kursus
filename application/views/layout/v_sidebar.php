ff7a6572f219807c1764142a7833a59daf05c002
unknown <odiksetiawan5@gmail.com>
2022-04-19 10:24:11 +0700

    adminlte3

--- application/views/layout/v_sidebar.php
+++ application/views/layout/v_sidebar.php
@@ -2,20 +2,14 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?php echo base_url();?>template/AdminLTE/index3.html" class="brand-link">
-      <img src="<?php echo base_url();?>template/AdminLTE/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <img src="<?php echo base_url();?>template/AdminLTE/dist/img/rr.png" alt="" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Kursus</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="<?php echo base_url();?>template/AdminLTE/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="<?php echo base_url();?>template/AdminLTE/#" class="d-block">Alexander Pierce</a>
-        </div>
       </div>
       <!-- Sidebar Menu -->
       <nav class="mt-2">
